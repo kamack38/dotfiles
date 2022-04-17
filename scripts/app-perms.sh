5465a79d9b51681f75957043e8acfe583b20f760
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-17 23:17:27 +0200

    🐛 fix: Fix brightness permissions

--- scripts/app-perms.sh
+++ scripts/app-perms.sh
@@ -5,4 +5,8 @@ CARD=$(ls /sys/class/backlight/ | awk '{print $1 }')
 
 sudo groupadd video
 sudo usermod -aG video $USER
-echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"$CARD\", GROUP=\"video\", MODE=\"0664\"" | sudo tee -a /etc/udev/rules.d/backlight.rules
+echo "RUN+=\"/bin/chgrp video /sys/class/backlight/$CARD/brightness\"" | sudo tee -a /etc/udev/rules.d/backlight.rules
+echo "RUN+=\"/bin/chmod g+w /sys/class/backlight/$CARD/brightness\"" | sudo tee -a /etc/udev/rules.d/backlight.rules
+
+# From Arch Wiki (doesn't work)
+# echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"$CARD\", GROUP=\"video\", MODE=\"0664\"" | sudo tee -a /etc/udev/rules.d/backlight.rules
