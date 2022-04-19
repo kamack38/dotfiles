d254e05a28f37916147dfd1f425b58c80bef140f
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-19 20:47:02 +0200

    feat: 🎸 Add KDE install srcipt

---
+++ scripts/kde.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+set -e
+
+#Default vars
+HELPER="paru"
+$HELPER -S --noconfirm --needed --quiet xorg \
+    plasma-meta \
+    kde-applications \
+    latte-dock
+
+# Enable services
+sudo systemctl enable ssdm
+sudo systemctl enable NetworkManager
+
+$HELPER -Rns --noconfirm konsole
