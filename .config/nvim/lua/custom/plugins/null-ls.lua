f0ec139338109ded1048aad41e93731eae651b28
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-05-03 20:43:04 +0200

    feat(nvim): 🎸 Add format on save

--- .config/nvim/lua/custom/plugins/null-ls.lua
+++ .config/nvim/lua/custom/plugins/null-ls.lua
@@ -25,6 +25,12 @@ M.setup = function()
    null_ls.setup {
       debug = true,
       sources = sources,
+      -- format on save
+      on_attach = function(client)
+         if client.resolved_capabilities.document_formatting then
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+         end
+      end,
    }
 end
 
