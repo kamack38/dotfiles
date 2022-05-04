8750fabb051518908339bc5d1f73f661778c998f
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-05-05 00:24:02 +0200

    feat(scripts): 🎸 Add blackarch & multilib repositories

--- scripts/asus.sh
+++ scripts/asus.sh
@@ -3,27 +3,28 @@
 HELPER="paru"
 
 $HELPER -S --noconfirm --needed --quiet asusctl-git \
-    alsa-tools \
-    optimus-manager \
-    optimus-manager-qt \
-    polybar
+	alsa-tools \
+	optimus-manager \
+	optimus-manager-qt \
+	polybar-git
 
 # Add asusctl repo
 if grep -Fxq "[g14]" /etc/pacman.conf; then
-    echo "g14 repo already exists"
+	echo "g14 repo already exists"
 else
-    sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+	sudo tee -a /etc/pacman.conf >/dev/null <<EOT
 [g14]
 SigLevel = DatabaseNever Optional TrustAll
 Server = https://arch.asus-linux.org
 EOT
 fi
+
 # Add archcraft repo
 if grep -Fxq "[archcraft]" /etc/pacman.conf; then
-    echo "Archcraft repo already exists"
+	echo "Archcraft repo already exists"
 else
-    sudo wget 'https://raw.githubusercontent.com/archcraft-os/core-packages/main/archcraft-mirrorlist/archcraft-mirrorlist' -O /etc/pacman.d/archcraft-mirrorlist
-    sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+	sudo wget 'https://raw.githubusercontent.com/archcraft-os/core-packages/main/archcraft-mirrorlist/archcraft-mirrorlist' -O /etc/pacman.d/archcraft-mirrorlist
+	sudo tee -a /etc/pacman.conf >/dev/null <<EOT
 [archcraft]
 SigLevel = Optional TrustAll
 Include = /etc/pacman.d/archcraft-mirrorlist
@@ -32,22 +33,29 @@ fi
 
 # Add chaotic aur
 if grep -Fxq "[chaotic-aur]" /etc/pacman.conf; then
-    echo "Chaotic-aur repo already exists"
+	echo "Chaotic-aur repo already exists"
 else
-    sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-    sudo pacman-key --lsign-key FBA220DFC880C036
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-    sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+	sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+	sudo pacman-key --lsign-key FBA220DFC880C036
+	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	sudo tee -a /etc/pacman.conf >/dev/null <<EOT
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
 EOT
 fi
 
 # Add multilib repo
-# sudo tee -a /etc/pacman.conf >/dev/null <<EOT
-# [multilib]
-# Include = /etc/pacman.d/mirrorlist
-# EOT
+if grep -Fxq "[multilib]" /etc/pacman.conf; then
+	echo "Multilib repo already exists"
+else
+	sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+EOT
+fi
+
+# Add blackarch repo
+curl -s https://blackarch.org/strap.sh | sudo bash
 
 # slow internet
 # sudo sysctl net.ipv4.tcp_ecn=0
