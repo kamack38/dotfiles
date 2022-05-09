e8b3b64f5e2ea9efb50cdc49797dd98dd379d15a
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-05-09 17:51:16 +0200

    feat(install): 🎸 Add more colurs & fix bugs

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,14 @@
 
 set -e
 # Colours
-BLACK='\033[0;30m'
-WHITE='\033[0;37m'
-BWHITE='\033[1;37m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Colour
+BLACK=$'\e[0;30m'
+WHITE=$'\e[0;37m'
+BWHITE=$'\e[1;37m'
+RED=$'\e[0;31m'
+BLUE=$'\e[0;34m'
+GREEN=$'\e[0;32m'
+YELLOW=$'\e[0;33m'
+NC=$'\e[0m' # No Colour
 
 # Default vars
 HELPER="paru"
@@ -19,24 +19,24 @@ neovimConfigDir="$HOME/.config/nvim"
 NVCHAD_URL="https://github.com/NvChad/NvChad"
 
 # Show greetings
-echo -e "   ___  __    _____ ______  ________  ________"
-echo -e "  |\  \|\  \ |\   _ \  _  \|\_____  \|\   __  \ "
-echo -e "  \ \  \/  /|\ \  \\\\\__\ \  \|_____\  \ \  \|\  \    Kamack38"
-echo -e "   \ \   ___  \ \  \\\\|__| \  \|______  \ \   __  \   ${BLUE}https://twitter.com/kamack38${NC}"
-echo -e "    \ \  \\\\ \  \ \  \    \ \  \| ____\  \ \  \|\  \  https://github.com/kamack38"
-echo -e "     \ \__\\\\ \__\ \__\    \ \__\|\_______\ \_______\ "
-echo -e "      \|__| \|__|\|__|     \|__|\|_______|\|_______|"
+echo "   ___  __    _____ ______  _________  ________"
+echo "  |\  \|\  \ |\   _ \  __ \|\_____   \|\   __  \ "
+echo "  \ \  \/  /|\ \  \\\\\__\ \  \|_____\  \ \  \|\  \    Kamack38"
+echo "   \ \   ___  \ \  \\\\|__| \  \|______  \ \   __  \   ${BLUE}https://twitter.com/kamack38${NC}"
+echo "    \ \  \\\\ \  \ \  \    \ \  \| ____\  \ \  \|\  \  https://github.com/kamack38"
+echo "     \ \__\\\\ \__\ \__\    \ \__\|\_______\ \_______\ "
+echo "      \|__| \|__|\|__|     \|__|\|_______|\|_______|"
 echo ""
-echo -e "		${RED}Thank you for using my script!${NC}"
+echo -e "		${RED}Thank you for using my script! ${NC}"
 
-echo -e "${BLUE}:: ${BWHITE}Doing a system update, cause stuff may break if it's not the latest version...${NC}"
+echo "${BLUE}:: ${BWHITE}Doing a system update, cause stuff may break if it's not the latest version...${NC}"
 sudo pacman --noconfirm -Syu
 
-echo -e "${BLUE}:: ${BWHITE}Installing basic packages...${NC}"
+echo "${BLUE}:: ${BWHITE}Installing basic packages...${NC}"
 sudo pacman -S --noconfirm --needed base-devel wget git
 
 # Create dirs
-echo -e "${BLUE}:: ${BWHITE}Creating directories...${NC}"
+echo "${BLUE}:: ${BWHITE}Creating directories...${NC}"
 mkdir -p $HOME/.local/share/fonts
 mkdir -p $neovimConfigDir
 mkdir -p $HOME/.srcs
@@ -121,17 +121,19 @@ fish -c 'nvm use lts && npm i -g carbon-now-cli \
 	npm-check-updates \
 	git-cz'
 
-echo -e "${GREEN}:: ${BWHITE}Installing ${BLUE}quokka.js plugins${NC}"
+echo "${GREEN}:: ${BWHITE}Installing ${BLUE}quokka.js plugins${NC}"
 fish -c 'npm i --prefix $HOME/.quokka dotenv-quokka-plugin \
 	jsdom-quokka-plugin'
 
 # Install spicetify-cli themes
-echo -e "${BLUE}:: ${BWHITE}Installing ${BLUE}spicetify themes${NC}"
+echo "${BLUE}:: ${BWHITE}Installing ${BLUE}spicetify themes${NC}"
 git clone https://github.com/spicetify/spicetify-themes.git $HOME/.config/spicetify/Themes/
 
 # Install NvChad
-if [[ -d $neovimConfigDir/.git && $(git -C $neovimConfigDir ls-remote --get-url) != $NVCHAD_URL ]]; then
-	echo -e "${BLUE}:: ${BWHITE}Installing ${BLUE}NvChad${NC}"
+if [[ -d "$neovimConfigDir/.git" && $(git -C $neovimConfigDir ls-remote --get-url) == $NVCHAD_URL ]]; then
+	echo "${YELLOW}:: ${BLUE}NvChad${BWHITE} is already installed${NC} -- skipping"
+else
+	echo "${BLUE}:: ${BWHITE}Installing ${BLUE}NvChad${NC}"
 	mv $neovimConfigDir $HOME/.config/NVIM.BAK
 	git clone https://github.com/NvChad/NvChad $neovimConfigDir --depth 1
 
@@ -139,74 +141,73 @@ if [[ -d $neovimConfigDir/.git && $(git -C $neovimConfigDir ls-remote --get-url)
 		+'autocmd User PackerComplete sleep 100m | write $HOME/.packer.sync.result | qall' \
 		+PackerSync
 	cat $HOME/.packer.sync.result | grep -v 'Press'
-else
-	echo -e "${YELLOW}:: ${BLUE}NvChad${BWHITE} is already installed!${NC}"
 fi
 
 # Set default shell to fish
 if [ ! "$(basename -- "$SHELL")" = "fish" ]; then
-	echo -e "${YELLOW}:: ${BWHITE}Setting default shell to ${BLUE}fish${NC}"
+	echo "${YELLOW}:: ${BWHITE}Setting default shell to ${BLUE}fish${NC}"
 	sudo chsh -s /bin/fish $USER
 fi
 
-echo -e "${YELLOW}:: ${BWHITE}Cloning ${BLUE}dotfiles${NC} from ${BLUE}${REPO#*//*/}${NC}"
+echo "${YELLOW}:: ${BWHITE}Cloning ${BLUE}dotfiles${NC} from ${BLUE}${REPO#*//*/}${NC}"
 git clone --bare $REPO $DOTFILES
 git --git-dir="$DOTFILES" --work-tree="$HOME" fetch --all
 git --git-dir="$DOTFILES" --work-tree="$HOME" config --local status.showUntrackedFiles no
 git --git-dir="$DOTFILES" --work-tree="$HOME" checkout --force
 
 # Update submodules
-echo -e "${BLUE}:: ${BWHITE}Updating ${BLUE}submodules${NC}"
+echo "${BLUE}:: ${BWHITE}Updating ${BLUE}submodules${NC}"
 git dtf submodule update --remote
 
-echo "Which DE do you want to install?"
-read -r -p "1) None 2) KDE 3) xfce (Default = 1): " de_script
+echo "${BLUE}:: ${BWHITE}Which DE do you want to install?${NC}"
+echo "	1) None 2) KDE 3) xfce"
+read -rp "Enter a number (default=1): " de_script
 
 case $de_script in
 
 2)
-	echo "Installing KDE..."
+	echo "${GREEN}:: ${BWHITE}Installing KDE..."
 	bash "$HOME/scripts/kde.sh"
 	;;
 
 3)
-	echo "Installing xfce..."
+	echo "${GREEN}:: ${BWHITE}Installing xfce..."
 	bash "$HOME/scripts/xfce.sh"
 	;;
 *)
-	echo "Skipping..."
+	echo "${YELLOW}:: ${BWHITE}Skipping..."
 	;;
 esac
 
-read -r -p "Do you want to setup additional programming fonts? [y/N] " fonts_setup
+read -rp "${BLUE}:: ${BWHITE}Do you want to setup additional programming fonts? [Y/n]${NC}: " fonts_setup
 
-if [[ $fonts_setup == y* ]]; then
+if [[ $fonts_setup != n* ]]; then
 	echo "Running font script..."
 	bash $HOME/scripts/fonts.sh
 fi
 
-read -r -p "Do you want to add additional pacman repositories (chaotic-aur, blackarch, multilib, archcraft)? [y/N] " repos_script
+read -rp "${BLUE}:: ${BWHITE}Do you want to add additional pacman repositories (chaotic-aur, blackarch, multilib, archcraft)? [y/N]${NC}: " repos_script
 
 if [[ $repos_script == y* ]]; then
 	echo "Running repos script..."
 	bash $HOME/scripts/repos.sh
 fi
 
-read -r -p "Do you want to run script for asus laptops? [y/N] " asus_script
+read -rp "${BLUE}:: ${BWHITE}Do you want to run script for asus laptops? [y/N]${NC}: " asus_script
 
 if [[ $asus_script == y* ]]; then
 	echo "Running asus script..."
 	bash $HOME/scripts/asus.sh
 fi
 
-read -r -p "Do you want to run script for razer hardware? [y/N] " razer_script
+read -rp "${BLUE}:: ${BWHITE}Do you want to run script for razer hardware? [y/N]${NC}: " razer_script
 
 if [[ $razer_script == y* ]]; then
 	echo "Running razer script..."
 	bash $HOME/scripts/razer.sh
 fi
 
-read -r -p "Do you want to reboot? [y/N] " reboot_prompt
+read -rp "${RED}:: ${BWHITE}Do you want to reboot? [y/N]${NC}: " reboot_prompt
 
 if [[ $reboot_prompt == y* ]]; then
 	echo "Rebooting..."
--- scripts/razer.sh
+++ scripts/razer.sh
@@ -6,3 +6,6 @@ echo "Installing razer drivers & RGB software..."
 $HELPER -S --noconfirm --needed --quiet polychromatic-git \
     openrazer-meta-git
 
+# Add current user to plugdev group
+CURRENT_USER="$USER"
+sudo gpasswd -a $CURRENT_USER plugdev
