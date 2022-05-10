a74704f4cc5b4613d74197c746599568c6a5d8fe
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-05-10 14:25:29 +0200

    feat(scripts): 🎸 Add gmail key to private files & fix missing files

--- scripts/export.sh
+++ scripts/export.sh
@@ -26,6 +26,9 @@ cp -f "$HOME/.wakatime.cfg" "$backupDir/.wakatime.cfg"
 # Copy ssh settings
 cp -rf "$HOME/.ssh" "$backupDir/.ssh"
 
+# Copy gmail key
+cp -rf "$HOME/.config/polybar/scripts/gmail/credentials.json" "$backupDir/.config/polybar/scripts/gmail/credentials.json"
+
 # Backup gpg keys
 gpg --output "$backupDir/.keys/backupkeys.pgp" --armor --export-secret-keys --export-options export-backup
 
--- scripts/import.sh
+++ scripts/import.sh
@@ -18,14 +18,20 @@ cp -rf "$backupDir/firefox/profiles.ini" "$firefoxConfigPath/profiles.ini"
 cp -rf "$backupDir/.config/ngrok" "$HOME/.config/ngrok"
 
 # Copy tokens/keys
-cp -rf "$HOME/.keys" "$backupDir/.keys"
+cp -rf "$backupDir/.keys" "$HOME/.keys"
+
+# Copy wakatime settings
+cp -f "$backupDir/.wakatime.cfg" "$HOME/.wakatime.cfg"
+
+# Copy ssh settings
+cp -rf "$backupDir/.ssh" "$HOME/.ssh"
+
+# Copy gmail key
+cp -rf "$backupDir/.config/polybar/scripts/gmail/credentials.json" "$HOME/.config/polybar/scripts/gmail/credentials.json"
 
 # Import gpg keys
-gpg --import "$backupDir/.keys/backupkeys.pgp"
-rm $backupDir/.keys/backupkeys.pgp
-
-# Copy tokens/keys
-cp -rf "$backupDir/.keys" "$HOME/.keys"
+gpg --import "$HOME/.keys/backupkeys.pgp"
+rm "$HOME/.keys/backupkeys.pgp"
 
 # Remove .backup directory
 rm -rf "$backupDir"
