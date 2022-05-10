03dd9068af102ff77c49ed9b6f17349e8ce16bfa
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-05-10 18:03:55 +0200

    fix(install): 🐛 Disable confirmation for adding chaotic-aur repository

--- scripts/repos.sh
+++ scripts/repos.sh
@@ -28,7 +28,7 @@ function chaotic_aur {
 		echo "${BLUE}:: ${BWHITE}Adding ${BLUE}chaotic-aur ${BWHITE}repository${NC}"
 		sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 		sudo pacman-key --lsign-key FBA220DFC880C036
-		sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+		sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 		sudo tee -a /etc/pacman.conf >/dev/null <<EOT
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
