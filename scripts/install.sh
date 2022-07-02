b105080ff1223bcde67fc3e66186ae980a4577bf
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-07-02 21:46:00 +0200

    feat(install): 🎸 Harden SSH & Setup iptables

--- scripts/install.sh
+++ scripts/install.sh
@@ -478,6 +478,53 @@ if [[ $harden == y* ]]; then
 	if [[ -f "$SSH_PATH" ]]; then
 		echo "${BLUE}:: ${BWHITE}Securing ${BLUE}${SSH_PATH}${BWHITE} permissions...${NC}"
 		sudo chmod 600 $SSH_PATH
+		if [[ ! $(sudo grep '^Protocol 2' $SSH_PATH) ]]; then
+			echo "${BLUE}:: ${BWHITE}Disabling SSH protocol v1...${NC}"
+			sudo tee -a $SSH_PATH >/dev/null <<EOT
+Protocol 2
+EOT
+		fi
+		if [[ ! $(sudo grep '^PermitRootLogin no' $SSH_PATH) ]]; then
+			echo "${BLUE}:: ${BWHITE}Disabling root login...${NC}"
+			sudo tee -a $SSH_PATH >/dev/null <<EOT
+PermitRootLogin no
+EOT
+		fi
+	fi
+
+	echo "${BLUE}:: ${BWHITE}Setting up ${BLUE}iptables${BWHITE}...${NC}"
+	sudo iptables -N TCP
+	sudo iptables -N UDP
+	sudo iptables -P FORWARD DROP
+	sudo iptables -P OUTPUT ACCEPT
+	sudo iptables -P INPUT DROP
+	sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	sudo iptables -A INPUT -i lo -j ACCEPT
+	sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+	sudo iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+	sudo iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+	sudo iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+	sudo iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+	sudo iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+	sudo iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+	sudo iptables -t raw -I PREROUTING -m rpfilter --invert -j DROP
+	sudo iptables -I TCP -p tcp -m recent --update --seconds 60 --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+	sudo iptables -D INPUT -p tcp -j REJECT --reject-with tcp-reset
+	sudo iptables -A INPUT -p tcp -m recent --set --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+	sudo iptables -I UDP -p udp -m recent --update --seconds 60 --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+	sudo iptables -D INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+	sudo iptables -A INPUT -p udp -m recent --set --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+
+	sudo iptables-save >/etc/iptables/iptables.rules
+	sudo systemctl enable --now iptables.service
+
+	read -rp "${BLUE}:: ${BWHITE}Do you want to scan your device now? [y/N]${NC}: " SCAN
+	if [[ $SCAN == y* ]]; then
+		echo "${BLUE}:: ${BWHITE}Running ${BLUE}lynis${BWHITE}...${NC}"
+		sudo lynis update info
+		sudo lynis audit system
+		echo "${BLUE}:: ${BWHITE}Checking for ${BLUE}root kits${BWHITE}...${NC}"
+		sudo rkhunter --check --sk
 	fi
 fi
 
