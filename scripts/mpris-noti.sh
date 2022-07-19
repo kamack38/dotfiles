2df470cce8c6a5a472b1d5eb78c0dfb23f863c67
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-07-19 15:43:34 +0200

    fix(scripts): 🐛 Add app name to mpris notification

--- scripts/mpris-noti.sh
+++ scripts/mpris-noti.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-notify-send -i $(playerctl metadata mpris:artUrl) "$(playerctl status): $(playerctl metadata xesam:artist) - $(playerctl metadata xesam:title)
+notify-send -i $(playerctl metadata mpris:artUrl) -a "Music Player" "$(playerctl status): $(playerctl metadata xesam:artist) - $(playerctl metadata xesam:title)
 Vol: $(($(playerctl volume | sed 's/0.//') / 10000))%"
