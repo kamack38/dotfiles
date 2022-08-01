9021edbc69d793792f20b69f33f4810f6cbc58a1
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-08-01 22:30:13 +0200

    feat(NeoVim): 🎸 Add todo comments plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -40,5 +40,11 @@ return {
       config = function()
          require("custom.plugins.null-ls").setup()
       end,
+   },
+   ["folke/todo-comments.nvim"] = {
+     requires = "nvim-lua/plenary.nvim",
+     config = function()
+       require("todo-comments").setup()
+     end
    }
 }
