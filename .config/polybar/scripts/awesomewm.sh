a941eabecfaa8093d5dfb683b432feab345c4da6
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-08-07 16:22:58 +0200

    feat(polybar): 🎸 Add more options to awesomewm module

--- .config/polybar/scripts/awesomewm.sh
+++ .config/polybar/scripts/awesomewm.sh
@@ -1,15 +1,28 @@
 #!/bin/env bash
 
-while getopts 'l' OPTION; do
+OUTPUT=""
+
+while getopts 'lf' OPTION; do
     case "$OPTION" in
     l)
         # Get current layout name
-        layout_name=$(awesome-client 'return mouse.screen.selected_tag.layout.name' | sed 's/string //; s/"//g')
-        echo ${layout_name^^}
+        layout_name=$(awesome-client 'return mouse.screen.selected_tag.layout.name' | sed 's/.*string //; s/"//g')
+        OUTPUT+="${layout_name^^}"
+        ;;
+    f)
+        # Show floating mode
+        floating_mode=$(awesome-client 'return mouse.current_client.floating' | sed 's/.*boolean //')
+        if [[ $floating_mode == 'true' ]]; then
+            OUTPUT+=":F"
+        fi
         ;;
     ?)
-        echo "script usage: $(basename $0) [-l]" >&2
+        echo "script usage: $(basename $0) [-l] [-f]" >&2
         exit 1
         ;;
     esac
 done
+
+if [[ "$OUTPUT" != "" ]]; then
+    echo "$OUTPUT"
+fi
