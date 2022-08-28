28db45162a754ab3e7e4b9bd751e4e39b0691be1
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-08-28 15:26:18 +0200

    refactor(setup): 💡 Wait after showing fstab

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -261,8 +261,9 @@ echo "keyserver hkp://keyserver.ubuntu.com" >>/mnt/etc/pacman.d/gnupg/gpg.conf
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
 genfstab -L /mnt >>/mnt/etc/fstab
-echo "${YELLOW}:: ${BWHITE}Generated /etc/fstab:${NC}" && sleep 1
+echo "${YELLOW}:: ${BWHITE}Generated /etc/fstab:${NC}"
 cat /mnt/etc/fstab
+sleep 2
 
 # Install GRUB
 if [[ ! -d "/sys/firmware/efi" ]]; then
