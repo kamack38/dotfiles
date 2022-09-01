08676e6c75a5621a4462c370cab3da1b500e12b0
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-09-01 15:03:56 +0200

    feat(NeoVim): 🎸 Add new keybindings

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,24 +1,36 @@
 local M = {}
 
 M.truzen = {
-    n = {
-        ["<leader>ta"] = { "<cmd> TZAtaraxis <CR>", "   truzen ataraxis" },
-        ["<leader>tm"] = { "<cmd> TZMinimalist <CR>", "   truzen minimal" },
-        ["<leader>tf"] = { "<cmd> TZFocus <CR>", "   truzen focus" },
-        ["<leader>tn"] = { "<cmd> TZNarrow <CR>", "   truzen narrow" },
-    },
+  n = {
+    ["<leader>ta"] = { "<cmd> TZAtaraxis <CR>", "   truzen ataraxis" },
+    ["<leader>tm"] = { "<cmd> TZMinimalist <CR>", "   truzen minimal" },
+    ["<leader>tf"] = { "<cmd> TZFocus <CR>", "   truzen focus" },
+    ["<leader>tn"] = { "<cmd> TZNarrow <CR>", "   truzen narrow" },
+  },
 }
 
 M.telescope = {
-    n = {
-        ["<leader>fp"] = { "<cmd> Telescope media_files <CR>", "  find media" },
-    },
+  n = {
+    ["<leader>fp"] = { "<cmd> Telescope media_files <CR>", "  find media" },
+  },
 }
 
 M.coderunner = {
-    n = {
-        ["<F7>"] = { "<cmd> RunCode <CR>", "Run code" },
-    }
+  n = {
+    ["<F7>"] = { "<cmd> RunCode <CR>", "Run code" },
+  },
+}
+
+M.trouble = {
+  n = {
+    ["<C-S-m>"] = { "<cmd> TroubleToggle <CR>", "Show diagnostics" },
+  },
+}
+
+M.codeactions = {
+  n = {
+    ["<C-space>"] = { "<cmd> CodeActionMenu <CR>", "Show code action menu" },
+  },
 }
 
 return M
