da4bdce4228d3e425898c7cc41e2a0459fd687a9
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-09-09 20:10:30 +0200

    feat(Polybar): 🎸 Update to use the NF 2.2.2

--- .config/polybar/scripts/openweathermap-simple.sh
+++ .config/polybar/scripts/openweathermap-simple.sh
@@ -5,49 +5,49 @@
 KEY_PATH="$HOME/.keys/openwheathermap.token"
 
 get_icon() {
-    case $1 in
-    # Icons for weather-icons
-    01d) icon="" ;;
-    01n) icon="" ;;
-    02d) icon="" ;;
-    02n) icon="" ;;
-    03*) icon="" ;;
-    04*) icon="" ;;
-    09d) icon="" ;;
-    09n) icon="" ;;
-    10d) icon="" ;;
-    10n) icon="" ;;
-    11d) icon="" ;;
-    11n) icon="" ;;
-    13d) icon="" ;;
-    13n) icon="" ;;
-    50d) icon="" ;;
-    50n) icon="" ;;
-    *) icon="" ;;
+	case $1 in
+	# Icons for weather-icons
+	01d) icon="" ;;
+	01n) icon="" ;;
+	02d) icon="" ;;
+	02n) icon="" ;;
+	03*) icon="" ;;
+	04*) icon="" ;;
+	09d) icon="" ;;
+	09n) icon="" ;;
+	10d) icon="" ;;
+	10n) icon="" ;;
+	11d) icon="" ;;
+	11n) icon="" ;;
+	13d) icon="" ;;
+	13n) icon="" ;;
+	50d) icon="" ;;
+	50n) icon="" ;;
+	*) icon="" ;;
 
-        # Icons for Font Awesome 5 Pro
-        #01d) icon="";;
-        #01n) icon="";;
-        #02d) icon="";;
-        #02n) icon="";;
-        #03d) icon="";;
-        #03n) icon="";;
-        #04*) icon="";;
-        #09*) icon="";;
-        #10d) icon="";;
-        #10n) icon="";;
-        #11*) icon="";;
-        #13*) icon="";;
-        #50*) icon="";;
-        #*) icon="";
-    esac
+		# Icons for Font Awesome 5 Pro
+		#01d) icon="";;
+		#01n) icon="";;
+		#02d) icon="";;
+		#02n) icon="";;
+		#03d) icon="";;
+		#03n) icon="";;
+		#04*) icon="";;
+		#09*) icon="";;
+		#10d) icon="";;
+		#10n) icon="";;
+		#11*) icon="";;
+		#13*) icon="";;
+		#50*) icon="";;
+		#*) icon="";
+	esac
 
-    echo $icon
+	echo $icon
 }
 
 if [ ! -f $KEY_PATH ]; then
-    echo '%{F#e56b6f}%{F-}'
-    exit 0
+	echo '%{F#e56b6f}%{F-}'
+	exit 0
 fi
 KEY=$(cat $KEY_PATH)
 CITY="Wroclaw"
@@ -57,27 +57,27 @@ SYMBOL="°"
 API="https://api.openweathermap.org/data/2.5"
 
 if [ -n "$CITY" ]; then
-    if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
-        CITY_PARAM="id=$CITY"
-    else
-        CITY_PARAM="q=$CITY"
-    fi
+	if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
+		CITY_PARAM="id=$CITY"
+	else
+		CITY_PARAM="q=$CITY"
+	fi
 
-    weather=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
+	weather=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
 else
-    location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+	location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
 
-    if [ -n "$location" ]; then
-        location_lat="$(echo "$location" | jq '.location.lat')"
-        location_lon="$(echo "$location" | jq '.location.lng')"
+	if [ -n "$location" ]; then
+		location_lat="$(echo "$location" | jq '.location.lat')"
+		location_lon="$(echo "$location" | jq '.location.lng')"
 
-        weather=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
-    fi
+		weather=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
+	fi
 fi
 
 if [ -n "$weather" ]; then
-    weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
-    weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
+	weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
+	weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "$(get_icon "$weather_icon")" "$weather_temp$SYMBOL"
+	echo "$(get_icon "$weather_icon")  $weather_temp$SYMBOL"
 fi
