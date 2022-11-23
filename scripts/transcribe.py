2f3717e6250bf3c1dab4d95d96a861ea72fce572
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-11-23 20:12:06 +0100

    feat(scripts): 🎸 Add Open Ai whisper simple script

---
+++ scripts/transcribe.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# usage: transcribe.py <input file> [output file]
+import whisper
+import sys
+
+if (len(sys.argv) < 2) :
+    print("usage: transcribe.py <input file> [output file]")
+    sys.exit(1)
+
+model = whisper.load_model("base")
+
+audio = sys.argv[1]
+transcribe = model.transcribe(audio, task="translate")
+
+if (len(sys.argv) >= 3) :
+    f = open(sys.argv[2], "a")
+    f.write(transcribe["text"])
+    f.close()
+else :
+    print(transcribe["text"])
