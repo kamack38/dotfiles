7343c05b13bc4a06c78e5c4e99693a95d70d20f2
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-02-19 15:18:58 +0100

    feat(scripts): 🎸 Add date to backup and fix backupkeys removal

--- scripts/export.sh
+++ scripts/export.sh
@@ -38,17 +38,18 @@ DIR_TO_BACKUP=(
 )
 
 # Create a tar archive
+BACKUP_PATH="$HOME/backup-$(date +%m-%Y).tar.gz"
 if ! command -v crabz &>/dev/null; then
 	echo "${YELLOW}:: ${BWHITE}It seems that you don't have ${BLUE}pigz${BWHITE} installed.${NC}"
 	echo "${YELLOW}:: ${BWHITE}Compression will be performed using only one core.${NC}"
 	tar -C "$HOME" -czvf "$HOME/backup.tar.gz" "${DIR_TO_BACKUP[@]}"
 else
 	echo "${BLUE}:: ${BWHITE}Compressing with multiple cores using ${BLUE}crabz${BWHITE}.${NC}"
-	tar -cf - -C "$HOME" --exclude='**node_modules' "${DIR_TO_BACKUP[@]}" | pv -s "$(du -sbc "${DIR_TO_BACKUP[@]}" | grep 'total' | awk '{ print $1 }')" | crabz >"$HOME/backup.tar.gz"
+	tar -cf - -C "$HOME" --exclude='**node_modules' "${DIR_TO_BACKUP[@]}" | pv -s "$(du -sbc "${DIR_TO_BACKUP[@]}" | grep 'total' | awk '{ print $1 }')" | crabz >"$BACKUP_PATH"
 fi
 
 # Clean up
 echo "${YELLOW}:: ${BWHITE}Cleaning up...${NC}"
-rm -f "$HOME/keys/backupkeys.pgp"
+rm -f "$HOME/.keys/backupkeys.pgp"
 
-echo "${GREEN}:: ${BWHITE}Your backup is in ${BLUE}${HOME}/backup.tar.gz${NC}"
+echo "${GREEN}:: ${BWHITE}Your backup is in ${BLUE}${BACKUP_PATH}${NC}"
