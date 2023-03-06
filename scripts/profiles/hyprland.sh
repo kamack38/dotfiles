4f7daa341ff231da7262d64b3869decce38e4654
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-03-06 20:14:30 +0100

    feat(Hyprland, scripts): 🎸 Add cliphist & fix hyprland script

--- scripts/profiles/hyprland.sh
+++ scripts/profiles/hyprland.sh
@@ -26,9 +26,9 @@ else
 fi
 
 HYPRLAND_PACKAGES=(
-	"hyprland-git"                    # A dynamic tiling Wayland compositor based on wlroots that doesn't sacrifice on its looks.
 	"hyprpaper-git"                   # A blazing fast wayland wallpaper utility with IPC controls.
 	"wl-clipboard"                    # Command-line copy/paste utilities for Wayland
+	"cliphist"                        # clipboard history "manager" for wayland
 	"grimblast-git"                   # A helper for screenshots within Hyprland.
 	"wlogout"                         # Logout menu for wayland
 	"waybar-hyprland-git"             # Highly customizable Wayland bar for Wlroots based compositors, with workspaces support for Hyprland
