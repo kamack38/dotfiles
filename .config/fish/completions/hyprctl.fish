72786da14f7ee84b838f712e9c710b33ef2bf2d0
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-04-16 16:14:55 +0200

    feat(fish): 🎸 Update hyprctl completion

--- .config/fish/completions/hyprctl.fish
+++ .config/fish/completions/hyprctl.fish
@@ -1,4 +1,5 @@
 # fish completion for hyprctl
+# command to convert markdown table: wl-paste | sed 's/| //; s/ |.*//' | tr '\n' ' ' | wl-copy
 
 function __hyprctl_get_clients_addresses
   hyprctl clients -j | jq '.[]|[.address]|@tsv' -r | sed 's/^/address:/'
@@ -10,16 +11,16 @@ set -l infocommands monitors workspaces clients activewindow layers devices acti
 
 set -l hyprland_sections general decoration animations input input_touchpad input_touchdevice gestures misc binds debug
 
-set -l general_options sensitivity border_size no_border_on_floating gaps_in gaps_out col.inactive_border col.active_border cursor_inactive_timeout layout no_cursor_wraps applay_sens_to_raw
-set -l decoration_options rounding multisample_edges active_opacity inactive_opacity fullscreen_opacity blur blur_size blur_passes blur_ignore_opacity blur_new_optimizations blur_xray drop_shadow shadow_render_power shadow_ingnore_window col.shadow col.shadow_inactive shadow_offset shadow_scale dim_inactive dim_strength dim_special dim_around screen_shader
+set -l general_options sensitivity border_size no_border_on_floating gaps_in gaps_out col.inactive_border col.active_border col.group_border col.group_border_active cursor_inactive_timeout layout no_cursor_warps no_focus_fallback apply_sens_to_raw resize_on_border extend_border_grab_area hover_icon_on_border
+set -l decoration_options rounding multisample_edges active_opacity inactive_opacity fullscreen_opacity blur blur_size blur_passes blur_ignore_opacity blur_new_optimizations blur_xray drop_shadow shadow_range shadow_render_power shadow_ignore_window col.shadow col.shadow_inactive shadow_offset shadow_scale dim_inactive dim_strength dim_special dim_around screen_shader
 set -l animations_options enabled
 set -l input_options kb_model kb_layout kb_variant kb_options kb_rules kb_file numlock_bydefault repeat_rat repeat_delay sensitivity accel_profile force_no_accel left_handed scroll_method scroll_button natural_scroll follow_mouse float_switch_override_focus
-set -l input_touchpad_options disable_while_typing natural_scroll scroll_factor middle_button_emulation clickfinger_behavior tap-to-click drag_lock 
+set -l input_touchpad_options disable_while_typing natural_scroll scroll_factor middle_button_emulation tap_button_map clickfinger_behavior tap-to-click drag_lock
 set -l input_touchdevice_options transform output
 set -l gestures_options workspace_swipe workspace_swipe_fingers workspace_swipe_distance workspace_swipe_invert workspace_swipe_min_speed_to_force workspace_swipe_create_new workspace_swipe_forever workspace_swipe_numbered
-set -l misc_options disable_hyprland_logo disable_splash_rendering no_vfr mouse_move_enables_dpms always_follow_on_dnd layers_hog_keyboard_focus animate_manual_resizes disable_autoreload enable_swallow swallow_regex focus_on_active no_direct_scanout
-set -l binds_options pass_mouse_when_bound scroll_event_delay workspace_back_and_forth allow_workspace_cycles
-set -l debug_options overlay damage_blink disable_logs disable_time damage_tracking
+set -l misc_options disable_hyprland_logo disable_splash_rendering vfr vrr mouse_move_enables_dpms key_press_enables_dpms always_follow_on_dnd layers_hog_keyboard_focus animate_manual_resizes animate_mouse_windowdragging disable_autoreload enable_swallow swallow_regex swallow_exception_regex focus_on_activate no_direct_scanout hide_cursor_on_touch mouse_move_focuses_monitor suppress_portal_warnings render_ahead_of_time render_ahead_safezone cursor_zoom_factor curzor_zoom_rigid
+set -l binds_options pass_mouse_when_bound scroll_event_delay workspace_back_and_forth allow_workspace_cycles focus_preferred_method
+set -l debug_options overlay damage_blink disable_logs disable_time damage_tracking enable_stdout_logs manual_crash
 
 # Disable file completions for the entire command
 complete -c hyprctl -f
