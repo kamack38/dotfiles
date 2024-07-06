fec78a16fec3f4fa49fab946f9f3c57d7d4caa21
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-07-07 00:57:50 +0200

    fix(NeoVim): 🐛 Fix lazy_path in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,7 @@ local lazy_path = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazy_path) then
   local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazy_path }
 end
 
 vim.opt.rtp:prepend(lazy_path)
