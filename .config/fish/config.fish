a044efba67bdbb868e26682897aa764c78f48b85
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-09-14 20:02:56 +0200

    fix(fish): 🐛 Fix the Ctrl+j key binding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -347,7 +347,7 @@ if status is-interactive
     if [ "$TERM" = linux ] && type -q starship
         source (starship init fish --print-full-init | psub)
     else if type -q oh-my-posh
-        oh-my-posh --init --shell fish --config '~/.config/oh-my-posh/kamack.omp.json' | source
+        oh-my-posh init fish --config '~/.config/oh-my-posh/kamack.omp.json' | source
     end
 end
 
@@ -357,3 +357,5 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+bind \cj history-search-forward
