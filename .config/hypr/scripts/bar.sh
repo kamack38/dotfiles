c6c09aef7d2bf6cb29d30e804dc17f04ef79943f
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-12-21 19:52:47 +0100

    fix: 🐛 Fix bar script

--- .config/hypr/scripts/bar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-~/.config}"
-
-if hyprctl monitors -j | jq -e '.[] | select(.name == "DP-1") | length > 0'; then
-	waybar -c "$XDG_CONFIG_HOME/waybar/config.jsonc"
-else
-	waybar -c "$XDG_CONFIG_HOME/waybar/laptop-config.jsonc"
-fi
--- .config/hypr/scripts/configure_monitors.sh
+++ .config/hypr/scripts/configure_monitors.sh
@@ -2,10 +2,13 @@
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-~/.config}"
 
+killall waybar
 if hyprctl monitors -j | jq -e '.[] | select(.name == "DP-1") | length > 0'; then
-	waybar -c "$XDG_CONFIG_HOME/waybar/config.jsonc"
+	waybar -c "$XDG_CONFIG_HOME/waybar/config.jsonc" &
+	disown
 else
-	waybar -c "$XDG_CONFIG_HOME/waybar/laptop-config.jsonc"
+	waybar -c "$XDG_CONFIG_HOME/waybar/laptop-config.jsonc" &
+	disown
 
 	# Leave empty space for the bar
 	hyprctl keyword workspace w[t1] m[0], gapsout:5 0 0 0
