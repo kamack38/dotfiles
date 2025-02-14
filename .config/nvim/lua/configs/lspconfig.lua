56b3a72d73bbe892e7348ab50b8ab4f6c1c93b88
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-02-14 20:52:30 +0100

    refactor(Neovim): 💡 Simplify lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,59 +1,42 @@
 local configs = require "nvchad.configs.lspconfig"
 
-local on_init = configs.on_init
-local capabilities = configs.capabilities
-local on_attach = configs.on_attach
-
-local lspconfig = require "lspconfig"
 local servers = {
-  "clangd",
-  "cssls",
-  -- "emmet_ls",
-  "html",
-  "nil_ls",
-  "ruff",
-  -- "tailwindcss",
-  "texlab",
-  "ts_ls",
-  "typos_lsp",
-}
-
-lspconfig.rust_analyzer.setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "rust" },
-  settings = {
-    ["rust_analyzer"] = {
-      cargo = {
-        allFeatures = true,
+  clangd = {},
+  cssls = {},
+  -- emmet_ls = {},
+  html = {},
+  nil_ls = {},
+  ruff = {},
+  -- tailwindcss = {},
+  texlab = {},
+  ts_ls = {},
+  typos_lsp = {},
+  rust_analyzer = {
+    filetypes = { "rust" },
+    settings = {
+      ["rust_analyzer"] = {
+        cargo = {
+          allFeatures = true,
+        },
       },
     },
   },
-}
-
-lspconfig.biome.setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
-}
-
-lspconfig.tinymist.setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
-  settings = {
-    exportPdf = "onType",
-    outputPath = "$root/target/$dir/$name",
+  biome = {
+    single_file_support = true,
   },
+  tinymist = {
+    single_file_support = true,
+    settings = {
+      exportPdf = "onType",
+      outputPath = "$root/target/$dir/$name",
+    },
+  }
 }
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup(opts)
 end
