fb2e619c939543722a30e749ae9c06202730991c
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-02-14 20:56:28 +0100

    feat(Neovim): 🎸 Show Nvdash when no bufs and restore cursor pos

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -102,3 +102,29 @@ autocmd("BufRead", {
     map("n", "<leader>np", package.change_version, { buffer = opts.buf, desc = "Package.json Change package version" })
   end,
 })
+
+-- Show Nvdash when no buffers are opened
+autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
+-- Restore cursor position on file open
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+        line > 1
+        and line <= vim.fn.line "$"
+        and vim.bo.filetype ~= "commit"
+        and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
