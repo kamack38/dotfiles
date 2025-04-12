d61cd15c3f229478116ea0fa2526b936c823fd6c
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-04-12 18:30:16 +0200

    feat: 🎸 Update the aegistoyubi script

--- scripts/aegistoyubi.sh
+++ scripts/aegistoyubi.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-jq -r '.db.entries[] | [.name, .issuer, .info.algo, .info.secret]| @tsv' <"$1" |
-	while IFS=$'\t' read -r name issuer algo secret; do
-		ykman oath accounts add -i "$issuer" -a "$algo" "$name" "$secret"
-		# echo "ykman oath accounts add -a \"$algo\" \"$name\" \"$secret\""
+jq -r '.db.entries[] | [.name, .issuer, .info.algo, .info.digits, .info.period, .info.secret]| @tsv' "$1" |
+	while IFS=$'\t' read -r name issuer algo digits period secret; do
+		ykman oath accounts add \
+			--issuer "$issuer" \
+			--algorithm "$algo" \
+			--digits "$digits" \
+			--period "$period" \
+			"$name" \
+			"$secret"
 	done
