7498f38388baaf1e6dc74fa858cb9b2d7ada225f
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-06-14 18:50:28 +0200

    feat(Neovim): 🎸 Add support for running c++ programs on Windows

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -462,7 +462,13 @@ return {
     cmd = { "RunCode", "RunFile", "RunProject" },
     opts = {
       filetype = {
-        cpp = 'cd "$dir" && mkdir -p "$dir/bin" && g++ "$dir/$fileName" -o "$dir/bin/$fileNameWithoutExt" -std=c++11 -fsanitize=address,undefined && "$dir/bin/$fileNameWithoutExt"',
+        cpp = function()
+          if vim.fn.has "win32" == 1 then
+            return 'cd "$dir" && mkdir -p "$dir/bin" -Force > $null && g++ "$dir\\$fileName" -std=c++11 -o "$dir\\bin\\$fileNameWithoutExt.exe" && & "$dir\\bin\\$fileNameWithoutExt.exe"'
+          else
+            return 'cd "$dir" && mkdir -p "$dir/bin" && g++ "$dir/$fileName" -o "$dir/bin/$fileNameWithoutExt" -std=c++11 -fsanitize=address,undefined && "$dir/bin/$fileNameWithoutExt"'
+          end
+        end,
         tex = 'mkdir -p "$dir/bin" && pdflatex -output-directory="$dir/bin" "$dir/$fileName"',
         rust = 'cargo run "$dir/$fileName"',
       },
