13eab9016bab8033d2446a60d633aab5da780bc5
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-06-23 01:00:02 +0200

    feat: 🎸 Enable the battery bar when on battery

--- .config/hypr/scripts/configure_monitors.sh
+++ .config/hypr/scripts/configure_monitors.sh
@@ -8,8 +8,13 @@ if hyprctl monitors -j | jq -e 'any(.[]; .name != "eDP-1")'; then
 	waybar -c "$XDG_CONFIG_HOME/waybar/config.jsonc" &
 	disown
 else
-	waybar -c "$XDG_CONFIG_HOME/waybar/laptop-config.jsonc" &
-	disown
+	if cat /sys/class/power_supply/AC/online | grep 0 >/dev/null 2>&1; then
+		waybar -c "$XDG_CONFIG_HOME/waybar/battery-config.jsonc" &
+		disown
+	else
+		waybar -c "$XDG_CONFIG_HOME/waybar/laptop-config.jsonc" &
+		disown
+	fi
 
 	# Leave empty space for the bar
 	hyprctl keyword workspace w[t1] m[0], gapsout:5 0 0 0
