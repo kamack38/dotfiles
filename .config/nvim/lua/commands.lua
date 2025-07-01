05994f6ca198feefc0d893cf1c1d722a680f8112
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-07-01 20:18:23 +0200

    fix: 🐛 Fix autocmds

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,6 +1,7 @@
 local create_cmd = vim.api.nvim_create_user_command
 local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+local map = vim.keymap.set
 
 -- Add alias for quit
 create_cmd("Q", "quit", {})
@@ -80,11 +81,11 @@ autocmd("BufReadPost", {
   callback = function()
     local line = vim.fn.line "'\""
     if
-      line > 1
-      and line <= vim.fn.line "$"
-      and vim.bo.filetype ~= "commit"
-      and vim.bo.filetype ~= "gitcommit"
-      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+        line > 1
+        and line <= vim.fn.line "$"
+        and vim.bo.filetype ~= "commit"
+        and vim.bo.filetype ~= "gitcommit"
+        and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
     then
       vim.cmd 'normal! g`"'
     end
@@ -143,5 +144,42 @@ autocmd("User", {
   desc = "Update git env for dotfiles after loading session",
   callback = function()
     update_git_env_for_dotfiles()
+  end,
+})
+
+-- Cargo.toml keybindigs
+augroup("Cargo.toml", {})
+autocmd("BufRead", {
+  group = "Cargo.toml",
+  pattern = "*Cargo.toml",
+  callback = function(opts)
+    local crates = require "crates"
+    map("n", "<leader>ct", crates.toggle, { buffer = opts.buf, desc = "Crates Toggle" })
+    map("n", "<leader>cr", crates.reload, { buffer = opts.buf, desc = "Crates Reload" })
+    map("n", "<leader>cv", crates.show_versions_popup, { buffer = opts.buf, desc = "Crates Versions popup" })
+    map("n", "<leader>cf", crates.show_features_popup, { buffer = opts.buf, desc = "Crates Features popup" })
+    map("n", "<leader>cd", crates.show_dependencies_popup, { buffer = opts.buf, desc = "Crates Dependencies popup" })
+    map("n", "<leader>cu", crates.update_crate, { buffer = opts.buf, desc = "Crates Update crate" })
+    map("n", "<leader>cU", crates.upgrade_crate, { buffer = opts.buf, desc = "Crates Upgrade crate" })
+    map("n", "<leader>ca", crates.update_all_crates, { buffer = opts.buf, desc = "Crates Update all crates" })
+    map("n", "<leader>cA", crates.upgrade_all_crates, { buffer = opts.buf, desc = "Crates Upgrade all crates" })
+    map("n", "<leader>cH", crates.open_homepage, { buffer = opts.buf, desc = "Crates Open homepage" })
+    map("n", "<leader>cR", crates.open_repository, { buffer = opts.buf, desc = "Crates Open repository" })
+    map("n", "<leader>cD", crates.open_documentation, { buffer = opts.buf, desc = "Crates Open documentation" })
+    map("n", "<leader>cC", crates.open_crates_io, { buffer = opts.buf, desc = "Crates Open crates.io" })
+  end,
+})
+
+-- package.json keybindigs
+augroup("package.json", { clear = true })
+autocmd("BufRead", {
+  group = "package.json",
+  pattern = "*package.json",
+  callback = function(opts)
+    local package = require "package-info"
+    map("n", "<leader>nu", package.update, { buffer = opts.buf, desc = "Package.json Update selected package" })
+    map("n", "<leader>nd", package.delete, { buffer = opts.buf, desc = "Package.json Delete selected package" })
+    map("n", "<leader>ni", package.install, { buffer = opts.buf, desc = "Package.json Install new package" })
+    map("n", "<leader>np", package.change_version, { buffer = opts.buf, desc = "Package.json Change package version" })
   end,
 })
