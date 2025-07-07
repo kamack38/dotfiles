29118c7c6b12b3c245c283b4f6f706b5e4a9cd69
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-07-08 00:19:50 +0200

    feat(battery): 🎸 Disable transparency in kitty when on battery

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
+
 HYPRGAMEMODE=${1:-$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')}
+KITTY_CONFIG="$HOME/.config/kitty/kitty.conf"
+
 if [ $HYPRGAMEMODE = 1 ]; then
 	# Disable unnecessary decorations
 	hyprctl --batch "\
@@ -13,7 +16,12 @@ if [ $HYPRGAMEMODE = 1 ]; then
 
 	# Switch to a different bar
 	~/.config/hypr/scripts/configure_monitors.sh
+
+	# Disable transparency in kitty
+	sed -i 's/background_opacity 0.8/background_opacity 1/' "$KITTY_CONFIG"
+
 	notify-send "Enabled gamemode"
 	exit
 fi
+sed -i 's/background_opacity 1/background_opacity 0.8/' "$KITTY_CONFIG"
 hyprctl reload
