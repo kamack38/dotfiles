90af106539c5130e3bcaba8e61a6c0686dee5bad
kamack38 <64226248+kamack38@users.noreply.github.com>
2025-09-17 20:22:09 +0200

    feat(Neovim): 🎸 Use LSP folding when possible

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -190,3 +190,14 @@ autocmd("FileType", {
     map("t", "<A-;>", "<A-;>", { buffer = opts.buf })
   end,
 })
+
+-- Use LSP folding when available
+autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
