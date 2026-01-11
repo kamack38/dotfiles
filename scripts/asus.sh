cda40abc494f1928e87f4cef16e0fdfbe9c7b4f7
kamack38 <64226248+kamack38@users.noreply.github.com>
2026-01-11 19:45:37 +0100

    fix(scripts): 🐛 Remove duplicated lines

--- scripts/asus.sh
+++ scripts/asus.sh
@@ -142,10 +142,6 @@ sudo tee /etc/modprobe.d/nvidia-pm.conf >/dev/null <<EOF
 options nvidia "NVreg_DynamicPowerManagement=0x03"
 EOF
 
-# Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-
 # Don't start irqbalance when on battery
 sudo mkdir -p /etc/systemd/system/irqbalance.service.d
 sudo tee /etc/systemd/system/irqbalance.service.d/override.conf >/dev/null <<EOF
